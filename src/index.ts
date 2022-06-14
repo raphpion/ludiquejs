adcca9c552798cd5955b3eafcbfe43513ec8d3db
Raphaël Pion <raph.pion@gmail.com>
2022-06-14 18:08:55 -0400

    #1 Create boilerplate function

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Says hello. This is the beginning of great things!
+ *
+ * @param {string} name The name of the person which you want to say hello to!
+ */
+export function hello_world(name: string): void {
+  console.log(`Hello ${name}`);
+}
